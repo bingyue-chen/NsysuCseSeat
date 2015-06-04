db08cd645e88311f644549e4dd54a51872b281b7
e19940604 <e19940604@gmail.com>
2015-06-04 17:17:48 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>NCS | Classroom </title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/index.css">
+	</head>
+	<body>
+	    <!-- Static navbar -->
+	    <nav class="navbar navbar-default navbar-static-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#">NsysuCseSeat</a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav">
+	            <li class="active"><a href="#">Home</a></li>
+	            <li><a href="#about">About</a></li>
+	            <li><a href="#contact">Contact</a></li>
+	            <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	              <ul class="dropdown-menu" role="menu">
+	                <li><a href="#">Action</a></li>
+	                <li><a href="#">Another action</a></li>
+	                <li><a href="#">Something else here</a></li>
+	                <li class="divider"></li>
+	                <li class="dropdown-header">Nav header</li>
+	                <li><a href="#">Separated link</a></li>
+	                <li><a href="#">One more separated link</a></li>
+	              </ul>
+	            </li>
+	          </ul>
+	          <ul class="nav navbar-nav navbar-right">
+	            <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>NsysuCseSeat</h1>
+        <p>Question , Meet and Chat ! </p>        
+        <p>How to use : </p>
+        <ol>
+        	<li>Select a classroom</li>
+        	<li>Choose a seat</li>
+        	<li>Start chatting , asking question or find the girl'FB who sat in front of you</li>
+        </ol>
+      </div>
+
+      <div class="row">
+      	<div class="col-md-4 classroom">
+      		<h3>F 5007</h3>
+      		<p>Student : </p>
+      		<p>Remaining seat : </p>
+      		<p>Now teaching : </p>
+      		<p><a class="btn btn-primary" href="#" role="button">Go to clasroom</a></p>
+      	</div>
+      </div>
+
+    </div> <!-- /container --x>
+	</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</html>
