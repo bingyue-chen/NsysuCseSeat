a9069833349d35ebda35029ed62507df47729d3b
XD <e19940604@gmail.com>
2015-06-17 14:52:27 -0400

    merge

--- js/5007.js
+++ js/5007.js
@@ -12,7 +12,7 @@ $(function(){
 	var seat = $(".seat");
 	var current_seat = -1 ;
 
-	var conn = new WebSocket('ws://localhost:8080');
+	var conn = new WebSocket('ws://seat.xgnid.me:8080');
 	conn.onopen = function(e) {
 	    console.log("Connection established!");
 	};
