d1b59100c84b68cab51b984c44948f056a2b1a0e
snow <snow.shanalike@gmail.com>
2015-06-23 22:52:57 +0800

    add empty msg check

--- index.php
+++ index.php
@@ -84,6 +84,9 @@
       </div>
 
     </div> <!-- /container -->
+    <section class="footer">
+		NsysuCseSeat 2015
+	</section>
 	</body>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
